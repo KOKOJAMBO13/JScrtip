84fb3855a94789fb8dbfdd84674c1100e49896ec
KOKOJAMBO13 <indexhtml1css2@gmail.com>
2025-04-09 14:06:25 +0500

    Add files via upload

---
+++ JAvaScriptme/script.js
@@ -0,0 +1,21 @@
+let ism = prompt("Ismingizni kiriting");
+let familya = prompt("Familyangizni kiriting");
+let sharif = prompt("Sharifingiz ?");
+let yoshingiz = prompt("Yoshingiz ?");
+let telefon = prompt("Telefon Raqamingiz ?");
+let manzil = prompt("Yashash manzilingiz ?");
+let ota = prompt("Otangizni Ismi ?");
+let ona = prompt("Onangizni ismi ?");
+let oqish = prompt("Maktabingiz raqami ?");
+let kurslar = prompt("Qanday kurslarga borasiz  ?");
+
+let javob = alert(
+  " Ismingiz : " +
+    ism +
+    " Familyangiz : " +
+    familya +
+    " Sharifingiz : " +
+    sharif +
+    " Yoshingiz : " +
+    yoshingiz 
+);
